19496a0f16747b9c0ce15bba37c0f352cb70ab67
chlgusrb97 <ikolp34@gmail.com>
2023-10-11 11:37:25 +0900

    feat: NewsLetter 유효성 검증

--- src/components/NewsLetter/NewsLetterSubscribe/NewsLetterSubscribe.tsx
+++ src/components/NewsLetter/NewsLetterSubscribe/NewsLetterSubscribe.tsx
@@ -1,24 +1,64 @@
+import {css} from "@emotion/react";
 import styled from "@emotion/styled";
+import {ChangeEvent, useState} from "react";
 
 const NewsLetterSubscribe = () => {
+  const [email, setEmailValue] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(false);
+
+  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {
+    const emailValue = e.target.value;
+    setEmailValue(emailValue);
+    validateEmail(emailValue);
+  };
+
+  const validateEmail = (emailValue: string) => {
+    const emailRegEx = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+    const testedEmailRegEx = emailRegEx.test(emailValue);
+
+    if (emailValue === "") {
+      setIsValidEmail(false);
+    } else {
+      setIsValidEmail(testedEmailRegEx);
+    }
+  };
+
+  console.log(isValidEmail);
+
   return (
     <SubscribeContainer>
       <Title>Subscribe to our newsletter</Title>
 
-      <InputBox>
-        <Input type="text" placeholder="Enter your email" />
+      <InputBox isValidEmail={isValidEmail} isNonValue={email !== ""}>
+        <Input
+          type="text"
+          placeholder="Enter your email"
+          value={email}
+          onChange={onChangeEmail}
+        />
 
-        <Submit type="submit">
+        <Submit
+          type="submit"
+          disabled={isValidEmail ? false : true}
+          isActive={isValidEmail ? false : true}
+        >
           <img src="/images/paper-plane.png" alt="전송아이콘" />
         </Submit>
       </InputBox>
+      {isValidEmail === false && email !== "" && (
+        <ErrorMessage>Please enter a valid email!</ErrorMessage>
+      )}
     </SubscribeContainer>
   );
 };
 
 export default NewsLetterSubscribe;
 
-const SubscribeContainer = styled.div``;
+const SubscribeContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Title = styled.h2`
   padding-bottom: 16px;
@@ -28,11 +68,14 @@ const Title = styled.h2`
   text-align: center;
 `;
 
-const InputBox = styled.div`
+const InputBox = styled.div<{isValidEmail: boolean; isNonValue: boolean}>`
   display: flex;
   width: 500px;
   height: 50px;
-  border: 1px solid #fff;
+  border: 1px solid
+    ${(props) =>
+      props.isValidEmail ? "#00c300" : props.isNonValue ? "#ff6633" : "#fff"};
+
   padding: 4px 10px 4px 4px;
   border-radius: 7px;
   background-color: rgba(255, 255, 255, 0.1);
@@ -42,7 +85,7 @@ const InputBox = styled.div`
 const Input = styled.input`
   width: 100%;
   height: 100%;
-  padding-left: 10px;
+  padding-left: 12px;
   color: #fff;
   background-color: transparent;
   font-family: Exo2-Regular;
@@ -53,13 +96,28 @@ const Input = styled.input`
   }
 `;
 
-const Submit = styled.button`
+const Submit = styled.button<{isActive: boolean}>`
   margin-left: 8px;
   background-color: transparent;
-  cursor: pointer;
+  cursor: ${(props) => (props.isActive ? "default" : "pointer")};
 
   > img {
     width: 32px;
     height: 32px;
+
+    ${(props) =>
+      props.isActive &&
+      css`
+        filter: opacity(0.5);
+      `}
   }
+`;
+
+const ErrorMessage = styled.p`
+  margin-top: 9px;
+  margin-left: 16px;
+  color: #ff6633;
+  font-size: 16px;
+  font-family: Exo2-Light;
+  letter-spacing: -0.24px;
 `;
