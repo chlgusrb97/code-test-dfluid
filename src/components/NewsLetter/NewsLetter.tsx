1296ec6e72c3033667cd67f864530ed34ea25089
chlgusrb97 <ikolp34@gmail.com>
2023-10-11 09:52:53 +0900

    feat: 이미지 API 적용 및 LocalStorage 캐싱

--- src/components/NewsLetter/NewsLetter.tsx
+++ src/components/NewsLetter/NewsLetter.tsx
@@ -1,10 +1,40 @@
 import styled from "@emotion/styled";
 import NewsLetterText from "./NewsLetterText/NewsLetterText";
 import NewsLetterSubscribe from "./NewsLetterSubscribe/NewsLetterSubscribe";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 const NewsLetter = () => {
+  const [imageData, setImageData] = useState("");
+  const accessKey = "RefQeWRu8Evi80g-fkUmTR_JgcD1xq5PQ_hpGVA05Ug";
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const cachedImageUrl = localStorage.getItem("randomImageUrl");
+
+        if (cachedImageUrl) {
+          setImageData(cachedImageUrl);
+        } else {
+          const result = await axios.get(
+            `https://api.unsplash.com/photos/random?client_id=${accessKey}`
+          );
+
+          const randomImageUrl = result.data.urls.full;
+
+          setImageData(randomImageUrl);
+          localStorage.setItem("randomImageUrl", randomImageUrl);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <NewsLetterWrapper>
+    <NewsLetterWrapper image={imageData}>
       <NewsLetterText />
 
       <NewsLetterSubscribe />
@@ -14,7 +44,7 @@ const NewsLetter = () => {
 
 export default NewsLetter;
 
-const NewsLetterWrapper = styled.div`
+const NewsLetterWrapper = styled.div<{image: string}>`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -23,7 +53,7 @@ const NewsLetterWrapper = styled.div`
   height: 740px;
   padding: 152px 80px;
   color: #fff;
-  background: rgba(0, 0, 0, 0.5) url("/images/banner.jpg") no-repeat center /
-    contain;
+  background: rgba(0, 0, 0, 0.5) url(${(props) => props.image}) no-repeat center /
+    cover;
   background-blend-mode: multiply;
 `;
